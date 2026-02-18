c52871aa304ba1a6b6f5028561e47e988bad7ddf
tyler-ham05 <tyjham05@gmail.com>
2026-02-17 17:42:26 -0800

    preprocessing and basic modeling

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from tensorflow.keras.models import load_model, Sequential
+from tensorflow.keras.layers import Input, Dense, Conv1D, MaxPooling1D, Flatten
+
+from preprocessing import *
+from preprocessing import *
+
+# directory to save model to (as a full path)
+save_to = '/Users/tyham/Documents/cmpm118-neuromorphic/Winter/results/'
+
+model = Sequential([
+    Input(shape=(200, 10)),   # (time, channels)
+
+    Conv1D(32, kernel_size=5, activation='relu'),
+    MaxPooling1D(pool_size=2),
+
+    Conv1D(64, kernel_size=5, activation='relu'),
+    MaxPooling1D(pool_size=2),
+
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(13, activation='softmax')
+])
+
+X_train, y_train, X_test, y_test, class_weights = preprocessing('s1/S1_A1_E1.mat')
+
+# insert your model here! 
+history, saved_model = train_model(model,X_train, y_train, X_test, y_test, save_to= save_to,class_weights= class_weights, epoch=30, )
+
